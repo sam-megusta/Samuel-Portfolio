54813dd344b5ba6017b493c002212d025c0753e3
Samuel Sumeet Raj Kumar <samuel.rajkumar@utdallas.edu>
2023-07-13 22:53:22 -0500

    Updates
    Pushing mods including links to projects and updated resume

--- src/app/profile/education/education.component.ts
+++ src/app/profile/education/education.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProfileService } from '../profile.service';
 
 @Component({
   selector: 'app-education',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./education.component.scss']
 })
 export class EducationComponent implements OnInit {
+  transcriptUrl: any
+  constructor(private profileService:ProfileService) { }
 
-  constructor() { }
+  //constructor() { }
 
   ngOnInit() {
+
+    this.transcriptUrl =  this.profileService.transcripturl
   }
 
 }
--- src/app/profile/profile.service.ts
+++ src/app/profile/profile.service.ts
@@ -19,7 +19,8 @@ export class ProfileService {
       title: 'Proof of Concept to Implement IBM Hyperledger Fabric over Traditional DBs for improved security standards for the protection of Customer PII Data',
       desc: '',
       imgUrl: 'assets/images/hyperledger.png',
-      tech: 'CouchDB, LevelDB, IBM Hyperledger Fabric'
+      tech: 'CouchDB, LevelDB, IBM Hyperledger Fabric',
+      mediumLink: ''
     },
 
     {
@@ -27,7 +28,8 @@ export class ProfileService {
       title: 'Automated Bot Notifications via Webex Integration for quicker defect resolution and automated control Processes',
       desc: '',
             imgUrl: 'assets/images/webex.png',
-      tech: 'Webex Bots, Springboot, MSSQL, API/Microservices'
+      tech: 'Webex Bots, Springboot, MSSQL, API/Microservices',
+      mediumLink: ''
     },
 
 
@@ -36,7 +38,8 @@ export class ProfileService {
       title: 'Step by Step Conceptual development (from Idea to UI design and ERD) of FitPal Application System',
       desc: '',
       imgUrl: 'assets/images/wnd.png',
-      tech: 'Class Diagrams, ERD, Windows Navigation Diagram (WND), Association Diagrams'
+      tech: 'Class Diagrams, ERD, Windows Navigation Diagram (WND), Association Diagrams',
+      mediumLink: 'https://drive.google.com/file/d/1C_Z8_CePm7Efh3qi4tbNTMSqK2I17K13/view?usp=sharing'
     },
 
 
@@ -45,7 +48,8 @@ export class ProfileService {
       title: 'Phrase Based Speech to Speech Machine Translator between 2 PCs from Hindi to Telugu and Vice-versa',
       desc: '',
       imgUrl: 'assets/images/mt.jpg',
-      tech: 'AI/ML, Deep learning techniques and Algorithms, Signal Processing'
+      tech: 'AI/ML, Deep learning techniques and Algorithms, Signal Processing',
+      mediumLink: ''
     },
 
     {
@@ -53,7 +57,8 @@ export class ProfileService {
       title: 'Data Visualization with Tableau',
       desc: '',
             imgUrl: 'assets/images/dv.png',
-      tech: 'Tableau,Spreadsheet Analytics'
+      tech: 'Tableau,Spreadsheet Analytics',
+      mediumLink: ''
     },
 
 
@@ -63,7 +68,8 @@ export class ProfileService {
       title: 'Client Server Bank Application using TCP and UDP',
       desc: '',
       imgUrl: 'assets/images/tcp.png',
-      tech: 'OSI, Computer Networking Fundamentals, TCP/UDP'
+      tech: 'OSI, Computer Networking Fundamentals, TCP/UDP',
+      mediumLink: ''
     },
 
 
@@ -73,7 +79,8 @@ export class ProfileService {
       title: 'Arduino Alarm Clock',
       desc: '',
       imgUrl: 'assets/images/ard.png',
-      tech: 'VHDL, Matlab, C Programming'
+      tech: 'VHDL, Matlab, C Programming',
+      mediumLink: ''
     },
 
     {
@@ -81,7 +88,8 @@ export class ProfileService {
       title: 'Personal Portfolio Using Angular',
       desc: '',
       imgUrl: 'assets/images/prtpic.png',
-      tech: 'Angular ,Bootstrap, NodeJS, TS, HTML, CSS'
+      tech: 'Angular ,Bootstrap, NodeJS, TS, HTML, CSS',
+      mediumLink: ''
     },
 
 
@@ -96,11 +104,12 @@ export class ProfileService {
   
   During my tenure as a Software Engineer at Ford, I have demonstrated excellence in rapid application development, error/defects reporting, automatic quote generation, as well as customer and business satisfaction. In addition, I have proven abilities in developing antifraud and AML services, database management, regression test scripts development, automation testing, bot notifications development, error handling and clean code management.`
 
-  about = "Curiosly involved in finding more efficient ways to do complex tasks. Meticulously calculated about my work and the approaches used in tackling a problem as I always have a can-do attitude. I am actively Seeking an opportunity for a summer 2023 internship in either software development, Quality Assurance, Product Management or Data Analytics ."
+  about = "Curiosly involved in finding more efficient ways to do complex tasks. Meticulously calculated about my work and the approaches used in tackling a problem as I always have a can-do attitude. I am actively Seeking an opportunity for a Fall 2023 internship in either software development, Quality Assurance, Product Management or Data/Market Research Analytics ."
 
   
   //resumeurl = "https://drive.google.com/file/d/14y1h8kIXf6HzoOMT24cTGzGxQbafCfjB/view"
   resumeurl =  "https://drive.google.com/file/d/1CDzRpDjE-JkiD89khiRV0xksNOH2NdPY/view"
+  transcripturl = "https://drive.google.com/file/d/1Se7P4-agmN82ulU0saij7i3o-0LNMzXH/view?usp=sharing"
 
   skillsData: any = [
     {
@@ -126,7 +135,7 @@ export class ProfileService {
     {
       'id': '5',
       'skill': 'DATA/BUSINESS ANALYTICS (R/Tableau/Splunk/Dynatrace/ETL)',
-      'progress': '81%'
+      'progress': '93%'
     },
     {
       'id': '6',
@@ -143,9 +152,10 @@ export class ProfileService {
       'education': 'Master\'s Degree',
       'stream': 'Master of Science in Information Technology and Management',
       'info': `UT DALLAS - Jindal School of Management .
-Finished the first Semester with a core GPA of 4.0 .
+Finished the first 2 Semesters with a core GPA of 4.0 .
 Subjects taken in the 1st Semester -> Systems Analysis & Project Management, Data Management, Statistics and Data Analysis .
 Subjects taken in the 2nd Semester -> AWS Cloud Solution Architecture, Data Vizualization, Web Analytics, Marketing Management .
+Subjects taken in the 3rd Semester -> OOPS with Python, Big Data, Foundations of Digital Product Management, Innovation within Corporations .
  .`,
       'institution': 'University of Texas at Dallas'
     },
@@ -167,7 +177,7 @@ Completed capstone Project "Implementing a Phrase Based Speech to Speech Machine
       location: 'Chennai, India',
       timeline: 'August 2019 to June 2022',
       role: 'Software Engineer',
-      work: 'Diagrammed and forecasted Network Architecture and Resource Planning for several applications for Ford Credit China with an accuracy of 100% .Ensured high client satisfaction by providing business teams dashboards visualizing data regarding application health and error metrics via Splunk & Dynatrace, and designed dashboards using Tableau describing customer vehicle preferences allowing business and manufacturing teams to produce more vehicles with said preferences .Developed Antifraud and AML services for Ford Credit China by consolidating essentials skills in spring framework . Improved automated quotes for Ford customers across the world EU, NA, SA, South Africa by developing several features for the Global Ford Credit Calculator (Global Ecommerce Platform) thereby increasing customer base by 45% .Simplified & elucidated error handling, API Metrics, codebase health monitoring, clean code management and similar healthy ethical coding practices by liaising with business teams all over the world (local to Ford Motors Pvt. Ltd.) to implement and incorporate cost-effective features as part of a multinational AGILE (SCRUM) team in a Product Driven Organization .Efficiently leveraged Webex applications to construct Bot Notifications to report errors/defects regarding the user-facing webpages as well as maintenance and execution of periodical batch operations reducing incident response time to under 5 minutes .Actively involved in the front-end development of web application on angular framework, middleware development of microservices/REST APIs on spring frameworks while designing database structures and stored procedures for backend processes with Oracle DB and SQL Servers .Proposed and constructed a proof of concept to integrate Hyperledger Fabric Blockchain for improved security and enhanced data management instead of traditional databases for the management of customer PII Data.'
+      work: 'Diagrammed and forecasted Network Architecture and Resource Planning for several applications for Ford Credit China with an accuracy of 100% using AWS CloudFormation. .Ensured high client satisfaction by providing business teams dashboards visualizing data regarding application health and error metrics via Splunk, AWS CloudWatch/Quicksight & Dynatrace, and designed dashboards using Tableau describing customer vehicle preferences thereby boosting production and driving sales by 4% in European markets. .Led the development of Antifraud and AML services for Ford Credit China by consolidating essential skills in spring framework while singlehandedly migrating Jenkins CICD pipeline from on-premises to GCP via IBM OpenShift reducing costs by 13% while increasing detection of fraudulent applications by 95%. .Improved automated quotes for Ford customers across the world EU, NA, SA, South Africa by developing several features for the Global Ford Credit Calculator (Global Ecommerce Platform) thereby increasing Ford Credit\'s customer base by 45% .Simplified & elucidated error handling, API Metrics, codebase health monitoring, clean code management and translated stakeholder requirements by collaborating with business teams all over the world to incorporate cost-effective features as part of a multinational AGILE (SCRUM) team in a Product Driven Organization reducing project costs by 32%. .Independently developed and implemented bot notifications integrating Webex and VictorOps, resulting in a significant reduction of incident downtime to under 5 minutes. .Developed and managed the front-end development of web applications on angular frameworks, middleware development of microservices/REST APIs on spring frameworks while designing database structures and stored/ETL procedures for backend processes with Oracle DB and SQL Servers while maintaining QA standards and DevOps pipeline thereby enhancing user engagement by 20%, improving system performance by 35%, 27% reduction in data retrieval time as well as automating the sign off procedures for the deployment pipeline reducing the TTL time by 50%.  .Proposed and constructed a proof of concept to integrate Hyperledger Fabric Blockchain for improved security and enhanced data management as compared to traditional databases for the management of customer PII Data annihilating the risk of data leaks completely. .Leveraged Selenium and JEST to develop and implement automated test scripts reducing time taken for UAT, Integration and regression testing by 40% while using JMeter for Load Tests. .Led Cloud development activities on PCF and AWS to cost optimize and trade capital expense for operational expense thereby saving Ford around 15000 USD per year (Hybrid Cloud/AWS Outposts for China). AWS Certified Cloud Practitioner. .Leveraged Selenium and JEST to develop and implement automated test scripts reducing time taken for UAT, Integration and regression testing by 40% while using JMeter for Load Tests. .Played a pivotal role as an ad-hoc product manager collaborating with 17 cross-functional, upstream and downstream teams to gather requirements, create and assign user stories, anchor backlog and sprint meetings, allocate funds, delineate project timelines and align contingencies through thorough risk analysis and mitigation, thereby driving deliverables for 12 Releases in the time span of 7 releases for the early launch of Ford\’s Ecommerce platform.  .Autonomously developed the frontend UI and backend for an administrative tool service to activate and deactivate vehicle models for company stakeholders having the necessary permissions as per the company\’s charter to bypass the natural batch ETL systems leveraging skills in AngularJS, CSS, jQuery and OracleDB.'
 
     },
     {
@@ -177,7 +187,7 @@ Completed capstone Project "Implementing a Phrase Based Speech to Speech Machine
       timeline: 'May 2018 to June 2018',
       role: 'Telcommunications Intern',
       work: `
-      Played a predominant role in the installation, commissioning and testing of remote VSAT system .
+      Played a predominant role in the installation, commissioning and testing of 15 remote VSAT systems. .
       Proactively involved in the operation & maintenance of Auto Tracking Antennas for Civil Aviation Oman.`
     },
 
